b8cfdd3e1eaa8d24772db162ba88915039dd9707
con7291 <con7291@utulsa.edu>
2024-10-12 21:37:39 -0500

    Add files via upload

---
+++ CHP4Tsk5/pfam.sh
@@ -0,0 +1,3 @@
+hmmpress /scratch/biol726309/db/pfam/Pfam-A.hmm
+
+pfam_scan.pl -fasta /scratch/biol726309/BIOL7263_Genomics/sequencing_data/ecoli/assembly/orfs.fa -dir  /scratch/biol726309/db/pfam/ -outfile /scratch/biol726309/BIOL7263_Genomics/sequencing_data/ecoli/assembly/contigs.orf.pfam -cpu 2 -as
