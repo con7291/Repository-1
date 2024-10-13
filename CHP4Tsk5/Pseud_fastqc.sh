00835d929f0368dd1d9a9ec61dfdd08fe0ce2afb
con7291 <con7291@utulsa.edu>
2024-10-12 21:57:48 -0500

    Delete CHP4Tsk5/Pseud_fastqc.sh

--- CHP4Tsk5/Pseud_fastqc.sh
+++
@@ -1,2 +0,0 @@
-fastqc /scratch/biol726309/BIOL7263_Genomics/pseudomonas_gm41/SRR491287_1.fastq.gz -o /scratch/biol726309/BIOL7263_Genomics/pseudomonas_gm41/
-fastqc /scratch/biol726309/BIOL7263_Genomics/pseudomonas_gm41/SRR491287_2.fastq.gz -o /scratch/biol726309/BIOL7263_Genomics/pseudomonas_gm41/
