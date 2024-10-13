6245abc7a91758cf32a0a19cc7cc7172f3b72f23
con7291 <con7291@utulsa.edu>
2024-10-12 22:01:01 -0500

    Delete CHP5Tsk4/long_assembly.sh

---
+++ GitRep.R
@@ -0,0 +1,33 @@
+# Repository-1
+Today we used Oscer to run jobs in the Terminal. Furthermore, we connected github repository with rstudio, and found the personal token is the key to avoiding repository issues of password authentication moving forward. Also, clicking on Repository, and changing default to yes for git allows project to show, then amend commit must be clicked on next screen to amend commitment.
+
+Class Project
+* [Research Proposal](https://github.com/con7291/Repository-1/blob/main/A%20Brief%20Research%20Proposal%20(4).pdf)
+
+Chapter 2
+* [ask 1](https://github.com/con7291/Repository-1/tree/666a8b7e5b181643edbdb47ed8bb2c95ded9a108/CHP2Tsk1)
+* [Task 2](https://github.com/con7291/Repository-1/tree/666a8b7e5b181643edbdb47ed8bb2c95ded9a108/CHP2Tsk2)
+* [Task 5](https://github.com/con7291/Repository-1/tree/666a8b7e5b181643edbdb47ed8bb2c95ded9a108/CHP2Tsk5)
+* [Task 6](https://github.com/con7291/Repository-1/tree/666a8b7e5b181643edbdb47ed8bb2c95ded9a108/CHP2Tsk6)
+* [Task 7](https://github.com/con7291/Repository-1/tree/666a8b7e5b181643edbdb47ed8bb2c95ded9a108/CHP2Tsk7)
+* [Task 8](https://github.com/con7291/Repository-1/tree/666a8b7e5b181643edbdb47ed8bb2c95ded9a108/CHP2Tsk8)
+* [Task 14](https://github.com/con7291/Repository-1/tree/4abab804d091b37a308fd60f0046da61bdf2074a/CHP2Tsk14)
+* [Task 15](https://github.com/con7291/Repository-1/tree/4abab804d091b37a308fd60f0046da61bdf2074a/CHP2Tsk15)
+
+Chapter 3
+* [Task 1](https://github.com/con7291/Repository-1/tree/4abab804d091b37a308fd60f0046da61bdf2074a/CHP3Tsk1)
+* [Task 3](https://github.com/con7291/Repository-1/tree/4abab804d091b37a308fd60f0046da61bdf2074a/CHP3Tsk3)
+* [Task 4](https://github.com/con7291/Repository-1/tree/4abab804d091b37a308fd60f0046da61bdf2074a/CHP3Tsk4)
+* [Task 6](https://github.com/con7291/Repository-1/tree/b720e0f86b5033995935c8d8ae19c590f180d0e1/CHP3Tsk6)
+* [Task 7](https://github.com/con7291/Repository-1/tree/b720e0f86b5033995935c8d8ae19c590f180d0e1/CHP3Tsk7)
+
+Chapter 4
+* [Task 1](https://github.com/con7291/Repository-1/tree/b720e0f86b5033995935c8d8ae19c590f180d0e1/CHP4Tsk1)
+* [Task 3](https://github.com/con7291/Repository-1/tree/4ed22ebcc8cf818f105e172aac51964c5b4173d2/CHP4Tsk3)
+* [Task 5](https://github.com/con7291/Repository-1/tree/4ed22ebcc8cf818f105e172aac51964c5b4173d2/CHP4Tsk5)
+
+Chapter 5
+* [Task 2](https://github.com/con7291/Repository-1/tree/4ed22ebcc8cf818f105e172aac51964c5b4173d2/CHP5Tsk2)
+* [Task 3](https://github.com/con7291/Repository-1/tree/4ed22ebcc8cf818f105e172aac51964c5b4173d2/CHP5Tsk3)
+* [Task 4](https://github.com/con7291/Repository-1/tree/4ed22ebcc8cf818f105e172aac51964c5b4173d2/CHP5Tsk4)
+* [Task 5](https://github.com/con7291/Repository-1/tree/4ed22ebcc8cf818f105e172aac51964c5b4173d2/CHP5Tsk5)
