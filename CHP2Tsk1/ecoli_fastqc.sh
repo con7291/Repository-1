d4ffd8bb3d57fb2db0691e9f96e34475652c6773
con7291 <con7291@utulsa.edu>
2024-10-12 21:35:51 -0500

    Add files via upload

---
+++ CHP2Tsk1/ecoli_fastqc.sh
@@ -0,0 +1,6 @@
+mkdir -p /scratch/biol726309/BIOL7263_Genomics/fastqc_output
+
+fastqc /scratch/biol726309/BIOL7263_Genomics/sequencing_data/ecoli/read_1.fastq.gz -o /scratch/biol726309/BIOL7263_Genomics/fastqc_output/
+fastqc /scratch/biol726309/BIOL7263_Genomics/sequencing_data/ecoli/read_2.fastq.gz -o /scratch/biol726309/BIOL7263_Genomics/fastqc_output/
+
+
